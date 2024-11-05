7655420e5233dc036f760bf72f0e465592952abe
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-11-04 21:11:55 -0800

    update: blogs coming soon

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,40 +11,36 @@ export default function BlogPosts() {
 
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">My Blog</h1>
+      <h1 className="mb-8 text-2xl font-medium tracking-tight">
+        Blogs Coming Soon
+      </h1>
       <div>
-        {allBlogs.length === 0 ? (
-          <p>No posts yet... Stay Tuned</p>
-        ) : (
-          <>
-            {allBlogs
-              .sort((a, b) => {
-                if (
-                  new Date(a.metadata.publishedAt) >
-                  new Date(b.metadata.publishedAt)
-                ) {
-                  return -1;
-                }
-                return 1;
-              })
-              .map((post) => (
-                <Link
-                  key={post.slug}
-                  className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80"
-                  href={`/blog/${post.slug}`}
-                >
-                  <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
-                    <p className="text-black dark:text-white tracking-tight">
-                      {post.metadata.title}
-                    </p>
-                    <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
-                      {formatDate(post.metadata.publishedAt, false)}
-                    </p>
-                  </div>
-                </Link>
-              ))}
-          </>
-        )}
+        {allBlogs
+          .sort((a, b) => {
+            if (
+              new Date(a.metadata.publishedAt) >
+              new Date(b.metadata.publishedAt)
+            ) {
+              return -1;
+            }
+            return 1;
+          })
+          .map((post) => (
+            <Link
+              key={post.slug}
+              className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80"
+              href={`/blog/${post.slug}`}
+            >
+              <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
+                <p className="text-black dark:text-white tracking-tight">
+                  {post.metadata.title}
+                </p>
+                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+                  {formatDate(post.metadata.publishedAt, false)}
+                </p>
+              </div>
+            </Link>
+          ))}
       </div>
     </section>
   );
