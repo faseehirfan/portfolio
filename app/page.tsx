b837b413aa037bbdb0995a41ce8ba3485d2c553d
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-11-05 06:02:47 -0800

    fix: optimize nextjs image

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ export default function Page() {
           src="/profile.png"
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
-          unoptimized
           width={160}
           height={160}
           priority
